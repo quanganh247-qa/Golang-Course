85ac5b77b19f9fb6de3fd17fe21e0f79632cb660
Dinh Huu Quang Anh <huuquanganhdinh@gmail.com>
2024-10-07 09:10:01 +0700

    Update code.go

--- 13-channels/exercises/4-buffered_channels/code.go
+++ 13-channels/exercises/4-buffered_channels/code.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// The addEmailsToQueue function tries to send all the emails to the channel emailsToSend synchronously, 
+// but the channel is unbuffered. This means that it blocks on the first emailsToSend <- email because there's no goroutine actively receiving from the channel.
+// Only after addEmailsToQueue finishes and returns the channel to the caller, the sendEmails function begins to receive from it.
+// But by that point, the sending has already stopped, resulting in a deadlock.
+
 func addEmailsToQueue(emails []string) chan string {
 	emailsToSend := make(chan string)
 	for _, email := range emails {
